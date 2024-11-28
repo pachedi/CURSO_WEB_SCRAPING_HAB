68dde85b235b258412588372682bee05b11b4d4c
pachedi <dapacheco.87@gmail.com>
2024-11-28 17:54:57 -0300

    de

--- Material-Sem-UBA-master/README.md
+++
@@ -1,1 +0,0 @@
-## Scripts Sem-UBA
--- taller-tweet-scraper-main/Scripts/encuentro_16_10_24/11.R
+++
@@ -1,6 +0,0 @@
-# Script 11 - Extracción de tweets de Timeline
-
-# Cargar la librería TweetScraperR
-library(TweetScraperR)
-
-(post_aas <- getTweetsTimeline("AAS_Sociologia", n_tweets = 400))
